514d4c0a26493076be51597a689628c9f928c4eb
michaelovo <emikeovo@gmail.com>
2022-02-05 13:27:59 +0100

    Retrieved and display restaurants list on home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,32 +3,63 @@
     <div>
         <h2>hello {{user_name}}</h2>
     </div>
+    <div>
+        <table class="table table-striped" border="1">
+            <thead>
+                <tr>
+                <th scope="col">SN</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Address</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in resturants" :key="item.id">
+                    <td>{{item.id}}</td>
+                    <td>{{item.name}}</td>
+                    <td>{{item.contact}}</td>
+                    <td>{{item.address}}</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 import Header from './Header.vue';
+import axios from 'axios';
     export default {
         name: 'Home',
         data () {
             return{
 
                 user_name:'',
+                resturants: [],
             }
         },
         components:{
             Header
         },
-        mounted(){
+       async mounted(){
             //prevent visiting homepage if not login
             let user = localStorage.getItem('signUp-info');
             this.user_name = JSON.parse(user).name;
             if(!user) {
                 this.$router.push({name:'SignUp'});
             }
+
+            let result = await axios.get("http://localhost:3000/resturants");
+            this.resturants = result.data;
+            //console.warn(result);
         }
     }
 </script>
 
-<style scoped>
-
+<style>
+    td{
+        width: 160px;
+        height: 14px;
+        text-align: left;
+    }
 </style>
