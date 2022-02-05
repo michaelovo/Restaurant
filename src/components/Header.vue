a77ff66201d29d28dc46986c8ef2bdf965407e89
michaelovo <emikeovo@gmail.com>
2022-02-05 06:02:07 +0100

    Added page header component

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="nav">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Services</a>
+        <a href="">Contact Us</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+    }
+</script>
+
+<style>
+.nav{
+    background-color: #333;
+    overflow: hidden;
+}
+.nav a{
+    float: left;
+    color: #f2f2f2;
+    padding: 14px 16px;
+    text-align: center;
+    font-size: 17px;
+    text-decoration: none;
+    margin-right: 5px;
+}
+.nav a:hover {
+    background-color: #ddd;
+    color: #333;
+}
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
+<Header/>
     <div>
         <h2>hello</h2>
     </div>
 </template>
 
 <script>
+import Header from './Header.vue';
     export default {
         name: 'Home',
+        components:{
+            Header
+        },
         mounted(){
             //prevent visiting homepage if not login
             let user = localStorage.getItem('signUp-info');
